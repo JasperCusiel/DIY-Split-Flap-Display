baa707db7d886447bcc16654733fd4dab0fe561b
Jasper Cusiel <64384723+JasperCusiel@users.noreply.github.com>
2021-08-25 16:48:33 +1200

    Create Constant_Speed.ino
    test code to verify every thing is working

---
+++ Arduino_test_code/Constant_Speed.ino
@@ -0,0 +1,16 @@
+// Constant Speed Stepper test 
+
+#include <AccelStepper.h>
+
+AccelStepper stepper(AccelStepper::FULL4WIRE, 5, 6, 7, 8); 
+
+void setup()
+{  
+   stepper.setMaxSpeed(1000);
+   stepper.setSpeed(175);	
+}
+
+void loop()
+{  
+   stepper.runSpeed();
+}
