0a1002dd8b4d3be434665c2bbc3cc0332b6c4a8c
Jasper Cusiel <64384723+JasperCusiel@users.noreply.github.com>
2021-09-14 09:41:36 +1200

    Create working(has_bugs).ino

---
+++ working(has_bugs).ino
@@ -0,0 +1,130 @@
+#include <AccelStepper.h>
+#include <math.h>
+
+const char *letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0","1", "2", "3", "4", "5", "6", "7", "8", "9","!","#", ",", " "};
+int positions[100] = {0};
+int num = 0;
+const char *to_find;
+int len = (sizeof (letters) / sizeof (*letters));
+int x;
+char buffer[128];
+String inData;
+int stepperPos;
+int index;
+float numSteps = 0;
+
+boolean movePastStart = false;
+
+#define CMDBUFFER_SIZE 32
+#define FULLSTEP 4
+
+int motorPin1 = 9; 
+int motorPin2 = 10; 
+int motorPin3 = 11; 
+int motorPin4 = 12; 
+
+AccelStepper stepper(FULLSTEP, motorPin1, motorPin3, motorPin2, motorPin4);
+
+
+void setup() {
+  Serial.begin(9600);
+  for (int i=1; i<41; i++) {
+    positions[i] = ((num += 51.2));
+    Serial.println(positions[i]);
+ }
+ 
+
+  stepper.setMaxSpeed(1000.0);
+  stepper.setAcceleration(10000);
+  stepper.setSpeed(1000);
+  homeDisplay();
+   
+}
+
+
+void loop() {
+  while (stepper.distanceToGo() > 0) {
+    stepper.run();
+  }
+  stepper.disableOutputs();
+    serialIn();
+  }
+
+void serialIn() {
+  static char cmdBuffer[CMDBUFFER_SIZE] = "";
+  char c;
+  if(Serial.available() > 0) {
+    c = processCharInput(cmdBuffer, Serial.read());
+    if (c == '\n') {
+      getLetterIndex(cmdBuffer);
+      cmdBuffer[0] = 0;
+    }      
+  }
+}
+
+void homeDisplay() {
+  int sensorPin = A6;
+  int sensorValue = 500;
+  pinMode(sensorPin, INPUT_PULLUP);
+  stepper.setSpeed(500);
+
+  while (sensorValue >= 200) {
+    sensorValue = analogRead(sensorPin);
+    //Serial.println(sensorValue);
+    stepper.runSpeed();
+  }
+}
+
+
+void getPositionByIndex(int index) {
+  int steps = positions[index];
+  Serial.println("no. Steps ");
+  Serial.println(steps);
+  numSteps = steps;
+  moveStepper();
+  
+}
+
+void moveStepper() {
+  if (numSteps <= stepper.currentPosition()) {
+    Serial.println("steps to 0 ");
+    Serial.println(2048 - (int(stepper.currentPosition())));
+    stepper.moveTo(2048 - (int(stepper.currentPosition())));
+    while (stepper.distanceToGo() > 0) {
+        stepper.run();
+      }
+    stepper.setCurrentPosition(0);
+    stepper.moveTo(numSteps);
+    while (stepper.distanceToGo() > 0) {
+        stepper.run();
+      }
+  }
+  else {
+    stepper.moveTo(numSteps);
+  }
+ 
+}
+
+void getLetterIndex(const char *letter) {
+  for (x = 0; x < len; x++) {
+    if (strcmp (letter, letters[x]) == 0) {
+      getPositionByIndex(x);
+    } 
+  }
+}
+
+char processCharInput(char* cmdBuffer, const char c) {
+  if (c >= 32 && c <= 126) {
+    if (strlen(cmdBuffer) < CMDBUFFER_SIZE) { 
+      strncat(cmdBuffer, &c, 1);   //Add it to the buffer
+    }
+    else {   
+      return '\n';
+    }
+  }
+  
+  else if ((c == 8 || c == 127) && cmdBuffer[0] != 0) {
+    cmdBuffer[strlen(cmdBuffer)-1] = 0;
+  }
+  return c;
+}
